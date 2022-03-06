90c7f8bc80e47e5b7af32b8517f86e2ace02367f
Andi Chaerul <andichaerul85@gmail.com>
2022-03-06 23:50:40 +0800

    css done

--- wp-content/themes/codingColective/index.php
+++ wp-content/themes/codingColective/index.php
@@ -2,13 +2,15 @@
 <html lang="en">
 
 <head>
-    <title>Bootstrap Example</title>
+    <!-- <title>Bootstrap Example</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
 </head>
 
@@ -39,39 +41,58 @@
         </div>
     </nav>
 
-    <div class="container cc3976524413">
-        <div class="row cc2791409718">
-            <img class="cc63" src="http://localhost:8888/wordpress/images/ccc.svg" />
-        </div>
+    <?php if (str_contains($_SERVER['REQUEST_URI'], 'home')) : ?>
+        <img class="cc63" src="http://localhost:8888/wordpress/images/ccc.svg" />
 
-        <div class="row cc37">
-            <div class="col-lg-6 col-md-12 col-sm-12 cc1082021360">
-                <img class="cc2406058084" src="http://localhost:8888/wordpress/images/img1.png" />
-                <img class="cc4003000486" src="http://localhost:8888/wordpress/images/img2.png" />
-                <img class="cc2568338324" src="http://localhost:8888/wordpress/images/img3.png" />
-                <img class="cc1062774977" src="http://localhost:8888/wordpress/images/group2.svg" />
-            </div>
-            <div class="col-md-6">
-                <div class="cc112">
-                    <div class="cc52">Singapore’s Leading</div>
-                    <div class="cc231">Consumer Electronics,</div>
-                    <div> IT & Furniture Store</div>
-                </div>
-                <div class="cc87">
-                    <img class="cc188" src="http://localhost:8888/wordpress/images/explore-more.svg">
+        <div class="container cc3976524413">
+            <div class="row cc1935295960">
+                <div class="col-md-6">
+                    <div class="cc1101706932">
+                        <img class="cc2406058084" src="http://localhost:8888/wordpress/images/img1.png" />
+                        <img class="cc2568338324" src="http://localhost:8888/wordpress/images/img3.png" />
+                        <img class="cc4003000486" src="http://localhost:8888/wordpress/images/img2.png" />
+                        <img class="cc1062774977" src="http://localhost:8888/wordpress/images/group2.svg" />
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div class="cc3612105092">
-        <img class="cc2766663593" src="http://localhost:8888/wordpress/images/321.svg" />
-        <img class="cc2291668064" src="http://localhost:8888/wordpress/images/vector1.svg" />
-    </div>
 
-    <footer>
+            <div class="cc112">
+                <div class="cc52">Singapore's Leading</div>
+                <div class="cc231">Consumer Electronics,</div>
+                <div> IT & Furniture Store</div>
+            </div>
+            <img class="cc188" src="http://localhost:8888/wordpress/images/explore-more.svg">
+            <img class="cc2766663593" src="http://localhost:8888/wordpress/images/321.svg" />
+            <img class="cc2291668064" src="http://localhost:8888/wordpress/images/vector1.svg" />
+        <?php endif; ?>
 
-    </footer>
+
+
+        <?php if (str_contains($_SERVER['REQUEST_URI'], 'sub-home')) : ?>
+            <div class="container cc3976524413"></div>
+        <?php endif; ?>
+        <footer>
+
+        </footer>
 
 </body>
+
+<script>
+    $(".cc188").click(function(e) {
+        e.preventDefault();
+        $(".cc1082021360").css("max-width", "100%");
+        $(".cc1082021360").css("min-width", "100%");
+        $(".cc1062774977").animate({
+            top: '0rem',
+            left: '63rem'
+        });
+
+        $(".cc2568338324").animate({
+            top: '0rem',
+            left: '0rem'
+        });
+
+    });
+</script>
 
 </html>
